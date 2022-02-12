bf141d0108b3df32dfdc24313a4bf276da27f5f5
Barsbarloo <lootanke@gmail.com>
2022-02-12 13:58:23 +0600

    Exer_4_2 add solution

--- Ruby_Recipes/exer_4_2.rb
+++ Ruby_Recipes/exer_4_2.rb
@@ -9,3 +9,36 @@
 # departure, and also one train id may be found in one file, but not another.
 # The exercise is to programmatically find the amount of time (in minutes) that each 
 # train stays in the station (when possible), and to flag the trains that have data anomalies.
+def timeinmin(tm)
+  a = tm.split(':')
+  a[0].to_i * 60 + a[1].to_i
+ end
+ def gethashfromfile(filename)
+  thefile = File.open(filename,'r')
+  h = Hash.new
+  while (line = thefile.gets)
+  x = line.chomp.split(/\s+/)
+  h[x[0]] = timeinmin(x[1])
+  end
+  thefile.close
+  h
+ end
+ h1 = gethashfromfile('exer_4_2_1.txt')
+ h2 = gethashfromfile('exer_4_2_2.txt')
+ k1 = h1.keys
+ k2 = h2.keys
+ kcommon = k1 & k2
+ knotinboth = (k1 - kcommon) | (k2 - kcommon)
+ arr = kcommon.to_a
+ for i in 0...arr.size
+  stay = h2[arr[i]] - h1[arr[i]]
+  if stay < 0
+  puts arr[i] + ': data issue'
+  else
+  puts arr[i] + ': stay ' + stay.to_s + ' minutes'
+  end
+ end
+ arr2 = knotinboth.to_a
+ for i in 0...arr2.size
+  puts arr2[i] + ': data issue'
+ end 

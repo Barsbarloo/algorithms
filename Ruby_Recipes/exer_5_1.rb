caf1dabd1d838307b7f0aabafc49a4c906f91f8a
Barsbarloo <lootanke@gmail.com>
2022-02-14 23:46:19 +0600

    Exer_5_1 add description

---
+++ Ruby_Recipes/exer_5_1.rb
@@ -0,0 +1,10 @@
+# You are given a hash in which the key is a student’s name and the value is the student’s 
+# total marks in an exam. Suppose anyone receiving more than 599 (i.e., 600 or more) is 
+# placed in the first division. Write a program to print the name and the marks of each 
+# student, in a nicely formatted manner, and include ‘First Division’ in the result if he/she 
+# achieved first division.
+# e.g given {"Abani Sen" => 650, "Dora Pridle" => 573}
+# It should print something like this:
+# Abani Sen : Marks obtained 650 : First Division
+# Dora Pridle : Marks obtained 573
+# Use at least one iterator in the solution.

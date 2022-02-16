897e3f5ba26978af58772ba938057c15331fc89c
Barsbarloo <lootanke@gmail.com>
2022-02-16 20:18:23 +0600

    Exer_5_3 add solution

--- Ruby_Recipes/exer_5_3.rb
+++ Ruby_Recipes/exer_5_3.rb
@@ -7,3 +7,23 @@
 # •	 801 onward: 70 credits
 # Write a program to determine and print the credits each of the students received 
 # (based on the hash in Exercise 5.2).
+h = {
+  "Abani Sen" => 650,
+  "Dora Pridle" => 573,
+  "Sana Chowdhury" => 824,
+  "Pritish Panda" => 732
+}
+h.map {|k,v|
+  case v
+  when 0..500
+  puts "#{k} : no credit"
+  when 501..600
+  puts "#{k} : credit : 10"
+  when 601..700
+  puts "#{k} : credit : 20"
+  when 701..800
+  puts "#{k} : credit : 40"
+  else
+  puts "#{k} : credit : 70"
+  end
+ }

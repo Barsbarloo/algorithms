da3301448986cab8d124db224e060c6d28bfb846
Barsbarloo <lootanke@gmail.com>
2022-02-16 20:10:39 +0600

    Exer_5_2 add solution

--- Ruby_Recipes/exer_5_2.rb
+++ Ruby_Recipes/exer_5_2.rb
@@ -6,3 +6,15 @@
 # "Pritish Panda" => 732
 # }
 # Print the name and marks, sorted by marks, with highest marks at the top
+h = {
+  "Abani Sen" => 650,
+  "Dora Pridle" => 573,
+  "Sana Chowdhury" => 824,
+  "Pritish Panda" => 732
+  }
+arr = h.invert.sort{|a,b| b<=>a}
+arr.each { |x|
+  str = ''
+  str = ' : First Division' if x[0] > 599
+  puts "#{x[1]} : Marks obtained #{x[0]}#{str}"
+}

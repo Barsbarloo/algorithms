16b603a9c3e37e0a9ae79e915708ef045bdf79fb
Barsbarloo <lootanke@gmail.com>
2022-02-16 20:03:24 +0600

    Exer_5_2 add description

---
+++ Ruby_Recipes/exer_5_2.rb
@@ -0,0 +1,8 @@
+#You are given the following hash.
+#h = {
+# "Abani Sen" => 650,
+# "Dora Pridle" => 573,
+# "Sana Chowdhury" => 824,
+# "Pritish Panda" => 732
+# }
+# Print the name and marks, sorted by marks, with highest marks at the top

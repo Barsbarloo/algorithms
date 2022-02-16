b4199f6d475e2b48803f1382de61aa8785be841e
Barsbarloo <lootanke@gmail.com>
2022-02-16 20:13:56 +0600

    Exer_5_3 add description

---
+++ Ruby_Recipes/exer_5_3.rb
@@ -0,0 +1,9 @@
+# For entry into engineering or medicine, when the score is calculated, some of the credit is 
+# taken from the marks in the exam discussed earlier. These are the rules:
+# •	 up to 500 marks: no credit toward entrance
+# •	 501 to 600: 10 credits
+# •	 601 to 700: 20 credits
+# •	 701 to 800: 40 credits
+# •	 801 onward: 70 credits
+# Write a program to determine and print the credits each of the students received 
+# (based on the hash in Exercise 5.2).
